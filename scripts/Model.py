1f2caef7a3524e62933926371ea35f85bd35471b
Mstavreff <mstavreff@outlook.com>
2025-05-11 19:42:49 +0200

    bigass model

--- scripts/Model.py
+++ scripts/Model.py
@@ -7,21 +7,23 @@ import seaborn as sns
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score, root_mean_squared_error, mean_squared_error
-import DataPipeline
-# %%
+import DataPipeline as pipeline
 np.random.seed(42)
+# %%
 
-import statsmodels as sm
+price_data = pipeline.data_preprocess(read=True)
+reference = pipeline.alchemy_preprocess(read=True)
+
 
 #440 1605 item id
-def TSRandomForest(regressors_main: pd.DataFrame, regressors_external: pd.DataFrame, window: int, n_splits, target_col, merge_on='timestamp', lag_features=[1,2],n_tree_estimators=100):
+def TSRandomForest(regressors_main: pd.DataFrame, regressors_external: list[pd.DataFrame], window: int, n_splits, target_col: pd.DataFrame, merge_on: str ='timestamp', lag_features: list[int] = [1,2],n_tree_estimators: int =100):
 
     for ext_df in regressors_external:
         regressors_main = regressors_main.merge(ext_df, on=merge_on, how='left') 
     for lag in lag_features:
          regressors_main[f'lag_{lag}'] = regressors_main[target_col].shift(lag)
     
-    regressors_main=regressors_main.dropna(inplace=True)
+    regressors_main.dropna(inplace=True)
 
     tscv = TimeSeriesSplit(n_splits=n_splits, max_train_size=window) 
     mae_scores = []
@@ -49,10 +51,24 @@ def TSRandomForest(regressors_main: pd.DataFrame, regressors_external: pd.DataFr
         mae_scores.append(mae)
         mse = mean_squared_error(y_test, y_pred)
         mse_scores.append(mse)
-        rmse = root_mean_squared_error(y_test, y_pred)
+        rmse = np.sqrt(mse)
         rmse_scores.append(rmse)
 
+        rss = np.sum((y_test - y_pred) ** 2)
 
+        # Number of parameters (including intercept-current price)
+        k = regressors_main.shape[1] -1 + 1  
+        n = len(y_train)  # Number of observations
+
+        # Compute AIC and BIC
+        aic = 2 * k + n * np.log(rss / n)
+        bic = k * np.log(n) + n * np.log(rss / n)
+
+        aic_scores.append(aic)
+        bic_scores.append(bic)
+
+        print(f"Split {len(mae_scores)} - AIC: {aic:.2f}")
+        print(f"Split {len(mae_scores)} - BIC: {bic:.2f}")
         print(f"Split {len(mae_scores)} - MAE: {mae:.2f}")
         print(f"Split {len(mae_scores)} - MAE: {mse:.2f}")
         print(f"Split {len(mae_scores)} - MAE: {rmse:.2f}")
