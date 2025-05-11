5dec6c0cae2fd73b68bca8f32174a128f186af38
Kiril Ivanov <kiril_ivanov_04@abv.bg>
2025-05-11 04:27:04 +0200

    di

--- ApiFetcher.py
+++ ApiFetcher.py
@@ -1,3 +1,4 @@
+from numpy import void
 import requests
 import pandas as pd
 from datetime import datetime
@@ -23,6 +24,25 @@ def fetch_latest_deprecated(item_ids: list[int]) -> pd.DataFrame:
         df['timestamp'] = pd.to_datetime(df['timestamp']).dt.tz_localize(None)
         df = df.reset_index()
         df = df.rename(columns={"index": "item_id"})
+        return df
+    else:
+        raise Exception("Failed to fetch data")
+
+def fetch_historical(item_id: int) -> pd.DataFrame:
+    url = f"https://api.weirdgloop.org/exchange/history/osrs/all?id={item_id}"
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        records = []
+        for item_id, entries in data.items():
+            for entry in entries:
+                records.append(entry)
+
+        df = pd.DataFrame(records)
+        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+        df["item_id"] = item_id
         return df
     else:
         raise Exception("Failed to fetch data")
@@ -59,36 +79,48 @@ def fetch_5min(timestamp: int = 0) -> pd.DataFrame:
     else:
         raise Exception("Failed to fetch data")
 
-def fetch_historical(item_id: int) -> pd.DataFrame:
-    url = f"https://api.weirdgloop.org/exchange/history/osrs/all?id={item_id}"
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-
-        records = []
-        for item_id, entries in data.items():
-            for entry in entries:
-                records.append(entry)
-
-        df = pd.DataFrame(records)
-        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-        df["item_id"] = item_id
-        return df
+def fetch_historical_5m(n = 10, mins=5, waits=1.1, timestamp: int = 0) -> pd.DataFrame:
+    if timestamp != 0:
+        unix_timestamp_seconds = timestamp
     else:
-        raise Exception("Failed to fetch data")
+        unix_timestamp_seconds = int(datetime.now().timestamp())
 
-def fetch_historical_5m(n = 10, mins=5, waits=1.1) -> pd.DataFrame:
-    unix_timestamp_seconds = int(datetime.now().timestamp())
     unix_timestamp_seconds = unix_timestamp_seconds - unix_timestamp_seconds % 300
-    df = pd.DataFrame(columns=['item_id', 'avgHighPrice', 'highPriceVolume', 'avgLowPrice', 'lowPriceVolume', 'timestamp'])
+    df = fetch_5min(unix_timestamp_seconds)
 
-    for t in range(0, n):
+    for t in range(1, n):
         df_t = fetch_5min(unix_timestamp_seconds - (mins * 60) * t)
         df = pd.concat([df, df_t], ignore_index=True)
         time.sleep(waits)
 
-    return df
+    return df[['item_id', 'avgHighPrice', 'highPriceVolume', 'avgLowPrice', 'lowPriceVolume', 'timestamp']]
+
+def writing_returns(filepath: str = "./data.csv", n: int = 100, p: int= 10) -> void:
+    timestampt_start = int(datetime.now().timestamp())
+    timestampt_start = timestampt_start - timestampt_start % 300
+    series_lenght = 0
+
+    with open("./data_properties.txt", "r") as file:
+            lines = file.readlines()
+    if lines != list():
+        timestampt_start = int(lines[0].replace("\n", ""))
+        series_lenght = int(lines[1].replace("\n", ""))
+
+    print(f"Initialized process. Expected mining time: {round(n * p * 1.1 / 60, 3)} minutes")
+    for t in range(1, p):
+        df_t = fetch_historical_5m(n = n, timestamp=timestampt_start - ((t * n) * 300))
+        last_call_timestamp = df_t.at[df_t.index[-1], 'timestamp']
+        df_t = df_t[['item_id', 'avgHighPrice', 'highPriceVolume', 'avgLowPrice', 'lowPriceVolume', 'timestamp']]
+        df_t.to_csv(filepath, mode='a', header=False, index=False)
+        series_lenght = series_lenght + n
+        with open("./data_properties.txt", "w") as file:
+            file.write(f"{last_call_timestamp}\n")
+            file.write(f"{series_lenght - 1}\n")
+        print(f"{(t + 1) * n} queries added!")
+    print("Success!")
+
+if __name__ == "__main__":
+    writing_returns(n=100, p=6)
 
 def fetch_historical_common_index() -> pd.DataFrame:
     url = f"https://api.weirdgloop.org/exchange/history/osrs/all?id=GE%20Common%20Trade%20Index"
@@ -200,7 +232,3 @@ def fetch_latest_idex_df():
     df = df.reset_index()
     del df['index']
     return df
-
-if __name__ == "__main__":
-    print(fetch_latest_idex_df())
-
